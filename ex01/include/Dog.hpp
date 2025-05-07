8287a33a489aae8774807dd8fd392c5c5c53f48c
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-07 15:27:21 +0200

    refactor: Add Dog class header and implementation files

---
+++ ex01/include/Dog.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 20:50:23 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/02 20:54:22 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+	public:
+		Dog();
+		~Dog();
+		void	makeSound() const;
+};
+
+#endif
