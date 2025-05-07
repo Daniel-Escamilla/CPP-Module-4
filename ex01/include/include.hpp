0d75ec76b43942223a5092a6347834073c9fb5e4
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-07 15:27:23 +0200

    feat: Add include.hpp file for common project includes

---
+++ ex01/include/include.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 19:50:27 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/02 19:50:28 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+# include "Animal.hpp"
+
+#endif
