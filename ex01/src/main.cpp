19c0ef260b2aa91337cc1ac9e9bcf415a48e7603
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-07 15:27:34 +0200

    refactor: Update main.cpp to use Animal, Cat, and Dog classes

---
+++ ex01/src/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 19:50:56 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/02 20:57:09 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Animal.hpp"
+#include "../include/Cat.hpp"
+#include "../include/Dog.hpp"
+
+int main()
+{
+	const Animal	*meta = new Animal();
+	const Animal	*j = new Cat();
+	const Animal	*i = new Dog();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	std::cout << meta->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+}
