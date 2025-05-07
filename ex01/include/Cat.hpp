f604b3cf7e7bcf6f9ff91711664cd0d00416a181
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-07 15:27:18 +0200

    refactor: Add Cat class header and implementation files

---
+++ ex01/include/Cat.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 20:54:27 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/02 20:55:09 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_HPP
+# define CAT_HPP
+
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+	public:
+		Cat();
+		~Cat();
+		void	makeSound() const;
+};
+
+#endif
