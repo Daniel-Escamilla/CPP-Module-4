e3ff81061890d65d6d7cef6b79ed2ce051d28a5b
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-09 18:21:35 +0200

    refactor: Update Cat.hpp to add copy constructor and assignment operator

--- ex00/include/Cat.hpp
+++ ex00/include/Cat.hpp
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 20:54:27 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/02 20:55:09 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/08 18:55:36 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ class Cat : public Animal
 	public:
 		Cat();
 		~Cat();
+		Cat(const Cat &copy);
+		Cat &operator=(const Cat &copy);
 		void	makeSound() const;
 };
 
