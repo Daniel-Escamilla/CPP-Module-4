f1bf3e033bc1f50ab50bda936d0eba856a4f1050
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-09 18:22:18 +0200

    refactor: Update main.cpp to improve animal instantiation and sound output

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 19:50:56 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/02 20:57:09 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:43:14 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@
 
 int main()
 {
-	const Animal	*meta = new Animal();
-	const Animal	*j = new Cat();
-	const Animal	*i = new Dog();
+	Animal* animals[15];
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	std::cout << meta->getType() << " " << std::endl;
-	i->makeSound();
-	j->makeSound();
-	meta->makeSound();
+	for (int i = 0; i < 5; ++i)
+		animals[i] = new Dog();
+	for (int i = 5; i < 10; ++i)
+		animals[i] = new Cat();
+	for (int i = 10; i < 15; i++)
+		animals[i] = new Animal();
+	for (int i = 0; i < 15; ++i)
+		animals[i]->makeSound();
+	for (int i = 0; i < 15; ++i)
+		delete (animals[i]);
+	return (0);
 }
