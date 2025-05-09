cdcc7d56c1db65f78d2ce342aa02463afc90e57f
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-09 18:22:15 +0200

    refactor: Improve Dog.cpp formatting and add copy constructor and assignment operator

--- ex01/src/Dog.cpp
+++ ex01/src/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 20:55:25 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/02 20:55:59 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:17:09 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,33 @@ Dog::Dog()
 {
 	type = "Dog";
 	std::cout << "Dog created" << std::endl;
+	brain = new Brain();
 }
 
-Dog::~Dog() { std::cout << "Dog destroyed" << std::endl; }
+Dog::~Dog()
+{
+	std::cout << "Dog destroyed" << std::endl;
+	delete brain;
+}
+
+Dog::Dog(const Dog &copy)
+	: Animal(copy)
+{
+	std::cout << "Dog copy constructor called" << std::endl;
+	type = copy.type;
+	brain = new Brain(*copy.brain);
+}
+
+Dog &Dog::operator=(const Dog &copy)
+{
+	std::cout << "Dog copy assignment operator called" << std::endl;
+	if (this != &copy)
+	{
+		Animal::operator=(copy);
+		*brain = *copy.brain; 
+	}
+	return (*this);
+}
 
 void	Dog::makeSound() const
 {
