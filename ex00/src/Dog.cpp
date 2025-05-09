3aa353cbe159b2044e4747357b418ec06743b2b7
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-09 18:21:49 +0200

    refactor: Enhance Dog class with copy constructor and assignment operator, and improve destructor formatting

--- ex00/src/Dog.cpp
+++ ex00/src/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 20:55:25 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/02 20:55:59 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/08 18:58:35 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,24 @@ Dog::Dog()
 	std::cout << "Dog created" << std::endl;
 }
 
-Dog::~Dog() { std::cout << "Dog destroyed" << std::endl; }
+Dog::~Dog()
+{
+	std::cout << "Dog destroyed" << std::endl;
+}
+
+Dog::Dog(const Dog &copy)
+{
+	std::cout << "Dog copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Dog &Dog::operator=(const Dog &copy)
+{
+	std::cout << "Dog copy assignment operator called" << std::endl;
+	if (this != &copy)
+		Animal::operator=(copy);
+	return (*this);
+}
 
 void	Dog::makeSound() const
 {
