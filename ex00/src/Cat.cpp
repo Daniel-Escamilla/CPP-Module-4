9a51fa265659878887703ea3650d4e9c04538c98
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-09 18:21:45 +0200

    refactor: Update Cat.cpp to improve destructor formatting and add copy constructor and assignment operator

--- ex00/src/Cat.cpp
+++ ex00/src/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 20:56:12 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/02 20:56:30 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/08 19:00:32 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,24 @@ Cat::Cat()
 	std::cout << "Cat created" << std::endl;
 }
 
-Cat::~Cat() { std::cout << "Cat destroyed" << std::endl; }
+Cat::~Cat()
+{
+	std::cout << "Cat destroyed" << std::endl;
+}
+
+Cat::Cat(const Cat &copy)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Cat &Cat::operator=(const Cat &copy)
+{
+	std::cout << "Cat copy assignment operator called" << std::endl;
+	if (this != &copy)
+		Animal::operator=(copy);
+	return (*this);
+}
 
 void	Cat::makeSound() const
 {
