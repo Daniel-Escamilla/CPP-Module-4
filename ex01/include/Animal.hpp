84e8cd34f28a5bd60d9115e0aaf8bf0a5ebd4b3f
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-09 18:21:54 +0200

    refactor: Update Animal.hpp to improve include directives and formatting

--- ex01/include/Animal.hpp
+++ ex01/include/Animal.hpp
@@ -6,14 +6,15 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 19:50:37 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/02 20:55:06 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:37:29 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef Animal_HPP
 # define Animal_HPP
 
-#include "include.hpp"
+# include <iostream>
+# include <string>
 
 class Animal
 {
@@ -22,7 +23,11 @@ class Animal
 	public:
 		Animal();
 		virtual ~Animal();
+		Animal(const Animal &copy);
+		Animal &operator=(const Animal &copy);
+	
 		std::string		getType() const;
+	
 		virtual void	makeSound() const;
 };
 
