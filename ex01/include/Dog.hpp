2507f4cebba892c0e919a847d3d571ff9b4f342b
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-09 18:22:03 +0200

    refactor: Update Dog.hpp to add copy constructor and assignment operator, and improve formatting

--- ex01/include/Dog.hpp
+++ ex01/include/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 20:50:23 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/02 20:54:22 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:14:34 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 # define DOG_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Dog : public Animal
 {
+	private:
+		Brain	*brain;
 	public:
 		Dog();
 		~Dog();
+		Dog(const Dog &copy);
+		Dog &operator=(const Dog &copy);
 		void	makeSound() const;
 };
 
