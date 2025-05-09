85e2343294ba9742b628eb9ddecb201903b67939
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-09 18:22:11 +0200

    refactor: Update Cat.cpp to improve formatting and add copy constructor and assignment operator

--- ex01/src/Cat.cpp
+++ ex01/src/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 20:56:12 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/02 20:56:30 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:16:19 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,33 @@ Cat::Cat()
 {
 	type = "Cat";
 	std::cout << "Cat created" << std::endl;
+	brain = new Brain();
 }
 
-Cat::~Cat() { std::cout << "Cat destroyed" << std::endl; }
+Cat::~Cat()
+{
+	std::cout << "Cat destroyed" << std::endl;
+	delete brain;
+}
+
+Cat::Cat(const Cat &copy)
+	: Animal(copy)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+	type = copy.type;
+	brain = new Brain(*copy.brain);
+}
+
+Cat &Cat::operator=(const Cat &copy)
+{
+	std::cout << "Cat copy assignment operator called" << std::endl;
+	if (this != &copy)
+	{
+		Animal::operator=(copy);
+		*brain = *copy.brain; 
+	}
+	return (*this);
+}
 
 void	Cat::makeSound() const
 {
